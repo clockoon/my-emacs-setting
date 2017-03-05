338dbb205043ee8b38caaa73d8b07cd83ede22a3
clockoon <clockoon@gmail.com>
2017-03-05 23:50:09 +0900

    add Youtube support

--- usr/orgmode.el
+++ usr/orgmode.el
@@ -11,3 +11,29 @@
           "* %? \n %U\n")
    ("j" "Journal-general" entry (file+datetree "~/org/journal.org")
         "* %?\nEntered on %U\n %i\n %a")))
+
+;; Additional macros
+; Youtube
+; ref: http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html
+; Usage: [[yt:"YoutubeID"]]
+
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))

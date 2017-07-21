dfe152e6e6958958e90745404f5d226f4f0b70a2
clockoon <clockoon@gmail.com>
2017-07-21 23:21:48 +0900

    adding private file (sample will be added)

--- 00-variables.el
+++ 00-variables.el
@@ -2,4 +2,11 @@
 ;; 00. variables
 ;; AUTHOR: Sungbin Jeon
 
+;; DESC: setting public / private variables and information; private things would be placed on private.el file
+
+;; NOTE: the sample of private.el is placed on private.sample.el file (personal information of me is not included)
+
+;; loading private stuffs
+(load-file (concat cfg-path "/private.el"))
+
 ;; loading variables

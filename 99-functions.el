3bedb72d954c0aa230454879bbf7cf9304ec3ae6
Sungbin Jeon <clockoon@gmail.com>
2018-03-16 08:12:52 +0900

    modifing font size selection function

--- 99-functions.el
+++ 99-functions.el
@@ -4,12 +4,28 @@
 
 
 ;; select fonts
-;;; ref: https://github.com/syl20bnr/spacemacs/issues/9445
+;;; refs: [1] https://github.com/syl20bnr/spacemacs/issues/9445
+;;;       [2] https://emacs.stackexchange.com/questions/28390/quickly-adjusting-text-to-dpi-changes
 (defun select-kr-font (opt)
   (when window-system
-   (if (> (x-display-pixel-width) 1600)
-	(setq x-font-height 16)
-      (setq x-font-height 12)))
+    (let* ((attrs (car (display-monitor-attributes-list)))
+         (size (assoc 'mm-size attrs))
+         (sizex (cadr size))
+         (res (cdr (assoc 'geometry attrs)))
+         (resx (- (cadr (cdr res)) (car res)))
+         (dpi (* (/ (float resx) sizex) 25.4)))
+    (cond
+     ((< dpi 110)
+      (setq x-font-height 16))
+     ((< dpi 130)
+      (setq x-font-height 18))
+     ((< dpi 160)
+      (setq x-font-height 20))
+      (t (setq x-font-height 22)))))
+	 
+;   (if (> (x-display-pixel-width) 1600)
+;	(setq x-font-height 16)
+;      (setq x-font-height 12)))
   (cond
    ((string= opt "c") ;; "c" means "codding"
     (set-face-attribute 'default nil :font

5b4d0526b8453f7e2bc78509a2c8cd6f2f6d0781
clockoon <clockoon@gmail.com>
2018-03-16 08:21:00 +0900

    adding Markdown mode

--- 02-packages.el
+++ 02-packages.el
@@ -114,6 +114,15 @@
   :ensure t
   )
 
+;;; markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 ;; 3) coding-related
 ;;; smartparens
 (use-package smartparens
